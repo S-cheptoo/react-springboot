fe5d18546548dff4748382705105f57e9ec5f23f
Sandra <cheptoosandra.t@gmail.com>
2022-07-23 16:50:48 +0300

    Get All

--- src/main/java/com/sandra/springboot/controller/StudentController.java
+++ src/main/java/com/sandra/springboot/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.sandra.springboot.controller;
 import com.sandra.springboot.model.Student;
 import com.sandra.springboot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -18,5 +17,10 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New student added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/com/sandra/springboot/service/StudentService.java
+++ src/main/java/com/sandra/springboot/service/StudentService.java
@@ -2,6 +2,9 @@ package com.sandra.springboot.service;
 
 import com.sandra.springboot.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
