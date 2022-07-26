8dc5662228eb08b4b51a55267febe960b6fb3eeb
Sandra <cheptoosandra.t@gmail.com>
2022-07-27 00:05:52 +0300

    React hook to post data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AppBar from "./components/SimpleAppBar"
+import Student from './components/student/Student';
 
 function App() {
   return (
     <div className="App">
       <AppBar/>
+      <Student/>
     </div>
   );
 }
