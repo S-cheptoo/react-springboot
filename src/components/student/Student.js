2aa0606cb635c2181105eedef94675e515817783
Sandra <cheptoosandra.t@gmail.com>
2022-07-27 01:20:51 +0300

    Completed

--- src/components/student/Student.js
+++ src/components/student/Student.js
@@ -4,49 +4,71 @@ import TextField from '@mui/material/TextField';
 import { Container} from '@mui/system';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+// import Paper from '@mui/material/Paper';
 
 export default function Student() {
     const paperStyle={padding:'50px 20px', width: 600,
     margin: "20px auto"}
     const [name,setName]= useState('')
     const [address,setAddress]=useState('')
+    cont [students,setStudents]=useState([])
     const handleClick=(e)=>{
         e.preventDefault()
         const student={name,address}
         console.log(student);
         fetch("http://localhost:8080/student/add", {
-        method: "POST",
-        headers: {"Content-Type":"application/json"},
-        body: JSON.stringify(student)
-    }).then(()=>{
-        console.log("New student is added.")
-    })
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(student)
+        }).then(()=>{
+            console.log("New student is added.")
+        })
     }
 
-  return (
-    <Container>
-        <Paper elevation={3} style={paperStyle}>
-            <h1 style={{color: "blue"}}><u>Add Student</u></h1>
-            <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1},
-            }}
-            noValidate
-            autoComplete="off"
-            >
-            <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
-            value={name} 
-            onChange={(e)=>setName(e.target.value)}/>
-            <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
-            value={address}
-            onChange={(e)=>setAddress(e.target.value)}/>
-            <Button variant="contained" onClick={handleClick}>Submit</Button>
-            </Box>
-            {name}
-            {address}
-        </Paper>
-    </Container>
-  );
+    useEffect(() => {
+        fetch("http://localhost:8080/student/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setStudents(result);
+        })
+    },[]);
+
+    return (
+            <Container>
+                <Paper elevation={3} style={paperStyle}>
+                    <h1 style={{color: "blue"}}><u>Add Student</u></h1>
+                    <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': { m: 1},
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    >
+                    <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+                    value={name} 
+                    onChange={(e)=>setName(e.target.value)}/>
+                    <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+                    value={address}
+                    onChange={(e)=>setAddress(e.target.value)}/>
+                    <Button variant="contained" onClick={handleClick}>Submit</Button>
+                    </Box>
+                </Paper>
+                <h1>Students</h1>
+
+                <Paper elevation={3} style={paperStyle}>
+
+                    {students.map(student=>(
+                        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
+                        Id:{student.id}<br/>
+                        Name:{student.name}<br/>
+                        Address:{student.address}<br/>
+                        <Paper/>
+                    ))
+                    }
+                </Paper>
+
+            </Container>
+    );
 }
